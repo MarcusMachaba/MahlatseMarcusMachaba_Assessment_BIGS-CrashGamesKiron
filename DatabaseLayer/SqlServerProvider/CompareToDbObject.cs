0eaed838c442bd14107033e0cc2c1175f88a1dee
Marcus Machaba <marcus@mvttech.co.za>
2025-07-08 14:28:27 +0200

    Add DAL remaining SqlProvider Classes

---
+++ DatabaseLayer/SqlServerProvider/CompareToDbObject.cs
@@ -0,0 +1,110 @@
+﻿using DatabaseLayer.Interfaces;
+using DatabaseLayer.Metadata;
+using DatabaseLayer.Metadata.Differences;
+using DatabaseLayer.SqlServerProvider.Metadata;
+using DatabaseLayer.SqlServerProvider.Metadata.StoredProcedures;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace DatabaseLayer.SqlServerProvider
+{
+    public static class CompareToDbObject
+    {
+        public static TableDifference Compare(
+          BaseDataProvider dataProvider,
+          TableMetadata tableMetadata,
+          string connectionString)
+        {
+            TableDifference tableDifference = new TableDifference(tableMetadata);
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                ((DbConnection)conn).Open();
+                DatabaseTable databaseTable = Provider.BuildDbTable(conn, dataProvider.GetTableName(tableMetadata.Type));
+                if (tableMetadata.PrimaryKeyProperty.Name.CompareTo(databaseTable.PrimaryKey) != 0)
+                    tableDifference.PrimaryKey = new Mismatch(tableMetadata.PrimaryKeyProperty.Name, databaseTable.PrimaryKey);
+                foreach (ColumnMetaData column in tableMetadata.Columns)
+                {
+                    ColumnMetaData col = column;
+                    bool columnChanged = false;
+                    DatabaseColumn dbCol = databaseTable.ColumnList.FirstOrDefault<DatabaseColumn>((Func<DatabaseColumn, bool>)(dbColumn => col.Name.ToLower().CompareTo(dbColumn.Name.ToLower()) == 0));
+                    if (dbCol == null)
+                    {
+                        tableDifference.NewColumns.Add(col);
+                    }
+                    else
+                    {
+                        Type enumType = col.DataType;
+                        bool flag = col.Required;
+                        if (enumType.IsGenericType && enumType.GetGenericTypeDefinition() == typeof(Nullable<>))
+                        {
+                            flag = false;
+                            enumType = enumType.GetGenericArguments()[0];
+                        }
+                        if (enumType.IsEnum)
+                            enumType = Enum.GetUnderlyingType(enumType);
+                        if (enumType != dbCol.DataType || flag != dbCol.Required || col.Length != dbCol.Length || col.Name != dbCol.Name)
+                            columnChanged = true;
+                        if (enumType == typeof(Decimal) && !columnChanged)
+                        {
+                            int? nullable1 = col.Precision;
+                            int? nullable2 = dbCol.Precision;
+                            if (nullable1.GetValueOrDefault() == nullable2.GetValueOrDefault() & nullable1.HasValue == nullable2.HasValue)
+                            {
+                                nullable2 = col.Scale;
+                                nullable1 = dbCol.Scale;
+                                if (nullable2.GetValueOrDefault() == nullable1.GetValueOrDefault() & nullable2.HasValue == nullable1.HasValue)
+                                    goto label_17;
+                            }
+                            columnChanged = true;
+                        }
+                    label_17:
+                        bool foreignKeyChanged = CompareToDbObject.CheckForeignKeyChanged(col, dbCol);
+                        if (columnChanged | foreignKeyChanged)
+                            tableDifference.ColumnDifferences.Add(new ColumnDifference(col, dbCol, columnChanged, foreignKeyChanged));
+                    }
+                }
+                tableDifference.AdditionalColumns = databaseTable.ColumnList.Where<DatabaseColumn>((Func<DatabaseColumn, bool>)(c => !tableMetadata.Columns.Any<ColumnMetaData>((Func<ColumnMetaData, bool>)(col => col.Name.ToLower() == c.Name.ToLower())) && c.Name != tableMetadata.PrimaryKeyProperty.Name)).ToList<DatabaseColumn>();
+                foreach (IStoredProcedure storedProcedure in Generator.GetStoredProcedures(tableMetadata, dataProvider))
+                {
+                    if (!StoredProcedureHelper.CheckStoredProcedure(conn, storedProcedure))
+                        tableDifference.StoredProcedureDifferences.Add(storedProcedure);
+                }
+            }
+            return tableDifference;
+        }
+
+        private static bool CheckForeignKeyChanged(ColumnMetaData col, DatabaseColumn dbCol)
+        {
+            bool flag = false;
+            if (col.HasForeignKey)
+            {
+                if (dbCol.ForeignKeyName == null)
+                    flag = true;
+                else if (col.ForeignKeyTable != dbCol.ForeignKeyTable || col.ForeignKeyColumn != dbCol.ForeignKeyColumn)
+                    flag = true;
+            }
+            else if (dbCol.ForeignKeyName != null)
+                flag = true;
+            return flag;
+        }
+
+        public static IndexDifference Compare(
+          BaseDataProvider dataProvider,
+          DbIndex index,
+          string connectionString)
+        {
+            IndexDifference indexDifference = new IndexDifference(index);
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                ((DbConnection)conn).Open();
+                indexDifference.DatabaseIndex = Provider.BuildIndex(conn, index.Name);
+            }
+            return indexDifference;
+        }
+    }
+}
---
+++ DatabaseLayer/SqlServerProvider/StoredProcedureHelper.cs
@@ -0,0 +1,73 @@
+﻿using DatabaseLayer.Interfaces;
+using DatabaseLayer.Metadata;
+using System;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+
+namespace DatabaseLayer.SqlServerProvider
+{
+    internal class StoredProcedureHelper
+    {
+        public static bool CheckStoredProcedure(SqlConnection conn, IStoredProcedure storedProcedure)
+        {
+            try
+            {
+                string result = string.Empty;
+                if (!StoredProcedureHelper.StoredProcedureExists(conn, storedProcedure.StoredProcedureName))
+                    return false;
+                using (SqlCommand command = conn.CreateCommand())
+                {
+                    ((DbCommand)command).CommandText = "sp_helptext";
+                    ((DbCommand)command).CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@ObjName", (object)storedProcedure.StoredProcedureName);
+                    using (SqlDataReader sqlDataReader = command.ExecuteReader())
+                    {
+                        while (((DbDataReader)sqlDataReader).Read())
+                            result = result + ((DbDataReader)sqlDataReader).GetString(0) + Environment.NewLine;
+                    }
+                }
+                return StoredProcedureHelper.SkeletenCompare(result, storedProcedure.StoredProcedureCreateText);
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private static bool StoredProcedureExists(SqlConnection conn, string storedProcedureName)
+        {
+            using (SqlCommand command = conn.CreateCommand())
+            {
+                ((DbCommand)command).CommandText = "SELECT COUNT(1) FROM sys.objects WHERE object_id = OBJECT_ID(@ObjectName) AND type in (N'P', N'PC')";
+                command.Parameters.AddWithValue("@ObjectName", (object)("[dbo].[" + storedProcedureName + "]"));
+                return ((DbCommand)command).ExecuteScalar() is int num && num > 0;
+            }
+        }
+
+        private static bool SkeletenCompare(string result, string value) => StoredProcedureHelper.SkeletanValue(result).CompareTo(StoredProcedureHelper.SkeletanValue(value)) == 0;
+
+        private static string SkeletanValue(string value) => value.Replace(Environment.NewLine, "").Replace(" ", "");
+
+        internal static void Deploy(
+          SqlConnection conn,
+          IStoredProcedure storedProcedure,
+          DeploySettings settings,
+          BaseDataProvider baseDataProvider)
+        {
+            if (StoredProcedureHelper.StoredProcedureExists(conn, storedProcedure.StoredProcedureName))
+            {
+                using (SqlCommand command = conn.CreateCommand())
+                {
+                    ((DbCommand)command).CommandText = "DROP PROCEDURE [" + storedProcedure.StoredProcedureName + "]";
+                    ((DbCommand)command).ExecuteNonQuery();
+                }
+            }
+            using (SqlCommand command = conn.CreateCommand())
+            {
+                ((DbCommand)command).CommandText = storedProcedure.StoredProcedureCreateText;
+                ((DbCommand)command).ExecuteNonQuery();
+            }
+        }
+    }
+}
