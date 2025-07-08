10a05cf97e8aa51b63279e3211db8717808fed96
Marcus Machaba <marcus@mvttech.co.za>
2025-07-08 13:59:38 +0200

    Add Attributes

---
+++ DatabaseLayer/Attributes/ColumnContractAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DatabaseLayer.Attributes
+{
+    public class ColumnContractAttribute : Attribute
+    {
+        public ColumnContractAttribute(params string[] joinedFrom) => JoinedFrom = joinedFrom;
+
+        public object DefaultValue { get; set; }
+
+        public Type ForeignKeyType { get; set; }
+
+        public int Length { get; set; }
+
+        public int Precision { get; set; }
+
+        public int Scale { get; set; }
+
+        public string[] JoinedFrom { get; private set; }
+
+        public bool Required { get; set; }
+
+        public bool Queryable { get; set; }
+
+        public bool IsTimeStamp { get; set; }
+
+        public string LengthString => Length != -1 ? Length.ToString() : "MAX";
+    }
+}
---
+++ DatabaseLayer/Attributes/TableContractAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DatabaseLayer.Attributes
+{
+    public class TableContractAttribute : Attribute
+    {
+        public TableContractAttribute() => this.FileGroup = "PRIMARY";
+
+        public string FileGroup { get; set; }
+
+        public string PartitionOn { get; set; }
+
+        public bool Audited { get; set; }
+
+        public string PrimaryKey { get; set; }
+    }
+}
