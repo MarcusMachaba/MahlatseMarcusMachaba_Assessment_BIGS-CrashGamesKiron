6024f235357f3a872a7028e201deca913e26f3b0
Marcus Machaba <marcus@mvttech.co.za>
2025-07-09 11:32:59 +0200

    Swap out inline sql with stored procs

---
+++ Applications/Common/DatabaseLayer.Idb/SQLConfigs/GetIndexInfo.sql
@@ -0,0 +1,20 @@
+﻿IF OBJECT_ID('dbo.GetIndexInfo', 'P') IS NOT NULL
+    DROP PROCEDURE dbo.GetIndexInfo;
+GO
+
+CREATE PROCEDURE dbo.GetIndexInfo 
+    @IndexName SYSNAME
+
+AS
+BEGIN
+    SET NOCOUNT ON;
+  
+    SELECT 
+        t.name      AS TableName,
+        ind.is_unique AS IsUnique
+    FROM sys.indexes ind
+    INNER JOIN sys.tables t 
+        ON ind.object_id = t.object_id
+    WHERE ind.name = @IndexName;
+END;
+GO
--- Applications/Common/DatabaseLayer.Idb/SqlServerProvider/Metadata/DatabaseIndexProvider.cs
+++ Applications/Common/DatabaseLayer.Idb/SqlServerProvider/Metadata/DatabaseIndexProvider.cs
@@ -1,4 +1,5 @@
 ﻿using DatabaseLayer.Metadata;
+using System.Data;
 using System.Data.Common;
 using System.Data.SqlClient;
 
@@ -6,49 +7,57 @@ namespace DatabaseLayer.SqlServerProvider.Metadata
 {
     internal static class DatabaseIndexProvider
     {
-        private const string GetIndexTableName = "SELECT t.name AS 'TableName', ind.is_unique AS 'IsUnique' FROM sys.indexes ind INNER JOIN sys.tables t ON ind.object_id = t.object_id WHERE ind.name = @IndexName";
-        private const string GetColumns = "\r\nSELECT \r\n    CASE is_descending_key WHEN 1 THEN 'DESC' ELSE 'ASC' END AS 'Sorting',\r\n\tcol.name AS 'Column'\r\nFROM \r\n    sys.indexes ind \r\nLEFT JOIN \r\n    sys.index_columns ic ON  ind.object_id = ic.object_id and ind.index_id = ic.index_id \r\nLEFT JOIN \r\n    sys.columns col ON ic.object_id = col.object_id and ic.column_id = col.column_id \r\nWHERE \r\n    ind.name = @IndexName\r\n    AND is_included_column = 0\r\nORDER BY \r\n    key_ordinal";
-        private const string GetIncludedColumns = "\r\nSELECT \r\n    col.name AS 'Column'\r\nFROM \r\n    sys.indexes ind \r\nLEFT JOIN \r\n    sys.index_columns ic ON  ind.object_id = ic.object_id and ind.index_id = ic.index_id \r\nLEFT JOIN \r\n    sys.columns col ON ic.object_id = col.object_id and ic.column_id = col.column_id \r\nWHERE \r\n    ind.name = @IndexName\r\n    AND is_included_column = 1\r\nORDER BY \r\n    key_ordinal";
-
         internal static DbIndex GetIndex(SqlConnection conn, string name)
         {
             DbIndex index;
-            using (SqlCommand command = conn.CreateCommand())
+
+            // 1) Table name + uniqueness
+            using (SqlCommand cmd = conn.CreateCommand())
             {
-                command.CommandText = "SELECT t.name AS 'TableName', ind.is_unique AS 'IsUnique' FROM sys.indexes ind INNER JOIN sys.tables t ON ind.object_id = t.object_id WHERE ind.name = @IndexName";
-                command.Parameters.AddWithValue("@IndexName", name);
-                using (SqlDataReader sqlDataReader = command.ExecuteReader())
+                cmd.CommandText = "dbo.GetIndexInfo";
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@IndexName", name);
+
+                using (var reader = cmd.ExecuteReader())
                 {
-                    if (!sqlDataReader.Read())
+                    if (!reader.Read())
                         return null;
-                    index = DbIndex.Create(((DbDataReader)sqlDataReader)["TableName"] as string, name);
-                    if ((bool)((DbDataReader)sqlDataReader)["IsUnique"])
+                    index = DbIndex.Create(reader["TableName"] as string, name);
+                    if ((bool)reader["IsUnique"])
                         index.IsUnique();
                 }
             }
-            using (SqlCommand command = conn.CreateCommand())
+
+            // 2) Key columns & sorting (ASC/DESC)
+            using (SqlCommand cmd = conn.CreateCommand())
             {
-                command.CommandText = "\r\nSELECT \r\n    CASE is_descending_key WHEN 1 THEN 'DESC' ELSE 'ASC' END AS 'Sorting',\r\n\tcol.name AS 'Column'\r\nFROM \r\n    sys.indexes ind \r\nLEFT JOIN \r\n    sys.index_columns ic ON  ind.object_id = ic.object_id and ind.index_id = ic.index_id \r\nLEFT JOIN \r\n    sys.columns col ON ic.object_id = col.object_id and ic.column_id = col.column_id \r\nWHERE \r\n    ind.name = @IndexName\r\n    AND is_included_column = 0\r\nORDER BY \r\n    key_ordinal";
-                command.Parameters.AddWithValue("@IndexName", name);
-                using (SqlDataReader sqlDataReader = command.ExecuteReader())
+                cmd.CommandText = "dbo.GetIndexColumns";
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@IndexName", name);
+
+                using (var reader = cmd.ExecuteReader())
                 {
-                    while (sqlDataReader.Read())
+                    while (reader.Read())
                     {
-                        if (((DbDataReader)sqlDataReader)["Sorting"] as string == "ASC")
-                            index.AddColumn(((DbDataReader)sqlDataReader)["Column"] as string);
+                        if (reader["Sorting"] as string == "ASC")
+                            index.AddColumn(reader["Column"] as string);
                         else
-                            index.AddColumnDesc(((DbDataReader)sqlDataReader)["Column"] as string);
+                            index.AddColumnDesc(reader["Column"] as string);
                     }
                 }
             }
-            using (SqlCommand command = conn.CreateCommand())
+
+            // 3) Included columns
+            using (SqlCommand cmd = conn.CreateCommand())
             {
-                command.CommandText = "\r\nSELECT \r\n    col.name AS 'Column'\r\nFROM \r\n    sys.indexes ind \r\nLEFT JOIN \r\n    sys.index_columns ic ON  ind.object_id = ic.object_id and ind.index_id = ic.index_id \r\nLEFT JOIN \r\n    sys.columns col ON ic.object_id = col.object_id and ic.column_id = col.column_id \r\nWHERE \r\n    ind.name = @IndexName\r\n    AND is_included_column = 1\r\nORDER BY \r\n    key_ordinal";
-                command.Parameters.AddWithValue("@IndexName", name);
-                using (SqlDataReader sqlDataReader = command.ExecuteReader())
+                cmd.CommandText = "dbo.GetIncludedIndexColumns";
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@IndexName", name);
+
+                using (var reader = cmd.ExecuteReader())
                 {
-                    while (sqlDataReader.Read())
-                        index.AddIncludedColumns(((DbDataReader)sqlDataReader)["Column"] as string);
+                    while (reader.Read())
+                        index.AddIncludedColumns(reader["Column"] as string);
                 }
             }
             return index;
