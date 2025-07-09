8178b621e036a2f8fca4e4c1c92e885b3b0d0cb6
Marcus Machaba <marcus@mvttech.co.za>
2025-07-09 18:31:45 +0200

    Add a debug tester consoleApp

---
+++ Applications/DebugTester/DataAccessLayer.Tests/Models/Bank.cs
@@ -0,0 +1,21 @@
+﻿using DatabaseLayer.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebugTester.DataAccessLayer.Tests.Models
+{
+    [TableContract(PrimaryKey = nameof(Bank.IdBank))]
+    public class Bank
+    {
+        [ColumnContract]
+        public int IdBank { get; set; }
+        [ColumnContract(Length = 100)]
+        public string Name { get; set; }
+
+        [ColumnContract(Length = 100)]
+        public string SecondName { get; set; }
+    }
+}
---
+++ Applications/DebugTester/Program.cs
@@ -0,0 +1,21 @@
+﻿using DebugTester.DataAccessLayer.Tests;
+using System;
+
+namespace DebugTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			try
+			{
+
+				DAL_Tester.TestCRUD();
+            }
+			catch (Exception e)
+			{
+				throw;
+			}
+        }
+    }
+}
