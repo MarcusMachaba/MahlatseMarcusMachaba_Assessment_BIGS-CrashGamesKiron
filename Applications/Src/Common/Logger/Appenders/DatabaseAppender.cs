76675d0daa28fab9faa6544cc88605782768c334
Marcus Machaba <marcus@mvttech.co.za>
2025-07-11 10:05:27 +0200

    Add lengths to logs model

--- Applications/Src/Common/Logger/Appenders/DatabaseAppender.cs
+++ Applications/Src/Common/Logger/Appenders/DatabaseAppender.cs
@@ -28,8 +28,8 @@ namespace Logger.Appenders
                         command.Parameters.Add(new SqlParameter("@Thread", SqlDbType.NVarChar, 255) { Value = loggingEvent.ThreadName });
                         command.Parameters.Add(new SqlParameter("@Level", SqlDbType.NVarChar, 50) { Value = loggingEvent.Level.Name });
                         command.Parameters.Add(new SqlParameter("@Logger", SqlDbType.NVarChar, 255) { Value = loggingEvent.LoggerName });
-                        command.Parameters.Add(new SqlParameter("@Message", SqlDbType.NVarChar, -1) { Value = loggingEvent.RenderedMessage });
-                        command.Parameters.Add(new SqlParameter("@Exception", SqlDbType.NVarChar, -1) { Value = loggingEvent.GetExceptionString() });
+                        command.Parameters.Add(new SqlParameter("@Message", SqlDbType.NVarChar, 4000) { Value = loggingEvent.RenderedMessage });
+                        command.Parameters.Add(new SqlParameter("@Exception", SqlDbType.NVarChar, 4000) { Value = loggingEvent.GetExceptionString() });
                         command.Parameters.Add(new SqlParameter("@MachineName", SqlDbType.NVarChar, 255) { Value = loggingEvent.LookupProperty("log4net:HostName") as string ?? Environment.MachineName });
 
                         command.ExecuteNonQuery();
